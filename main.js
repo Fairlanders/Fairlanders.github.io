c392e19e7ccbc0be50367a40aef8236f592c0347
Uğur Taşdan <42900996+Fairlanders@users.noreply.github.com>
2019-06-01 21:03:42 +0300

    Last version of my work. gotta do some more stuff but its enough for today.
    Okay.

---
+++ main.js
@@ -0,0 +1,58 @@
+AOS.init();
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200); // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200); // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function () { // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0 // Scroll to top of body
+    }, 500);
+});
+$(".first-page-chevron,.button,.second").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#second-page").offset().top
+    },
+        'slow');
+});
+$(".first").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#first-page").offset().top
+    },
+        'slow');
+});
+$(".third,.second-page-chevron").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#third-page").offset().top
+    },
+        'slow');
+});
+$(".forth,.third-page-chevron").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#forth-page").offset().top
+    },
+        'slow');
+});
+$(".fifth,.forth-page-chevron").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#fifth-page").offset().top
+    },
+        'slow');
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 30) {
+            $("nav").css({ "background": "#fff", "box-shadow": "0px 2px 8px 0px rgba(50, 50, 50, 0.08)" });
+            $('nav i').css('color', '#504b4b');
+        }
+        else {
+            $("nav").css({ "background": "transparent", "box-shadow": "none" });
+            $('nav i').css('color', '#fff');
+        }
+    });
+});
